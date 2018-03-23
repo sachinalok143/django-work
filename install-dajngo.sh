0e91fa47b2c35e2899004e04004f689a4e4e65d9
sachin kumar <sachinalok143@gmail.com>
2018-03-24 01:48:23 +0530

    Update install-dajngo.sh

--- install-dajngo.sh
+++ install-dajngo.sh
@@ -1,7 +1,9 @@
 
-pip install --upgrade pip
+pip install django==1.8
+sudo su
 pip install --upgrade django-crispy-forms
 pip install django-registration
+exit
 sudo apt-get install --upgrade python3
 sudo apt-get update
 sudo apt-get install -y python3-pip
