7edc3df5dc29484d590e02bf65d5dcf807ca78ce
sachin kumar <sachinalok143@gmail.com>
2018-03-24 01:39:29 +0530

    Create install-dajngo.sh

---
+++ install-dajngo.sh
@@ -0,0 +1,8 @@
+
+pip install --upgrade pip
+pip install --upgrade django-crispy-forms
+pip install django-registration
+sudo apt-get install --upgrade python3
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo pip install django-angular
